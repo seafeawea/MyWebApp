f2718af06f7877588d0560bfbcf8e7a439a149ec
seafeawea <452576504@qq.com>
2015-06-18 15:14:48 +0800

    Add configurations

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'XZ'
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'xz',
+        'password': 'interact',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
