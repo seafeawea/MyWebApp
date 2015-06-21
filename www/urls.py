a9179823565302407a5f98a929e4dcf9b2c98f7a
seafeawea <452576504@qq.com>
2015-06-21 19:51:20 +0800

    add api support

--- www/urls.py
+++ www/urls.py
@@ -11,6 +11,8 @@ import logging
 
 from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
 
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
+
 from models import User, Blog, Comment
 
 
@@ -18,6 +20,14 @@ from models import User, Blog, Comment
 @get('/')
 def index():
     blogs = Blog.find_all()
-    users = User.find_first('where email=?', 'admin@example.com')
-    return dict(blogs=blogs, users=users)
+    user = User.find_first('where email=?', 'admin@example.com')
+    return dict(blogs=blogs, user=user)
 
+
+@api
+@get('/api/users')
+def api_get_users():
+    users = User.find_by('order by created_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
